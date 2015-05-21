f1652753a514dbb891c97ffc03a75bcd9cfeaa82
nicosio2 <nico.gruendel@web.de>
2015-05-21 21:44:22 +0200

    Basic Layout

---
+++ Tanzroboter Ai Library/src/main/java/org/pixelgaffer/turnierserver/tanzroboter/ailibrary/App.java
@@ -0,0 +1,13 @@
+package org.pixelgaffer.turnierserver.tanzroboter.ailibrary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Tanzroboter Game Logic/src/main/java/org/pixelgaffer/turnierserver/tanzroboter/gamelogic/App.java
@@ -0,0 +1,13 @@
+package org.pixelgaffer.turnierserver.tanzroboter.gamelogic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
